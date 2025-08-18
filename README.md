851bcbd9b422681ed5f79296224fdc5719ad7ad2
Junhsiun <xjuunn@gmail.com>
2025-08-18 19:14:50 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# NeonHalo
---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: false },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  css: ['./app/assets/main.css'],
+  modules: [
+    '@nuxt/icon',
+    '@pinia/nuxt',
+    '@vueuse/nuxt',
+    '@nuxtjs/color-mode'
+  ]
+})
